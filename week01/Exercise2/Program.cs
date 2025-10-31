5493fc4173ce090f80cad01d35b19e8acd9065b7
Tasha-Vidal <tvidalbravo@byupathway.edu>
2025-10-30 21:39:23 -0500

    finished prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Asks the user for their rating percentage
+        Console.WriteLine("Enter your grade percentage:");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+       string lettergrade = "";
+        
+
+        if (percentage >= 90)
+        {
+            lettergrade= "A";
+        }
+        else if (percentage >= 80)
+        {
+            lettergrade= "B";
+        }
+        else if (percentage >= 70)
+        {
+            lettergrade= "C";
+        }
+        else if (percentage >= 60)
+        {
+            lettergrade= "D";
+        }
+        else
+        {
+            lettergrade= "F";
+        }
+        Console.WriteLine($"Your grade is: {lettergrade}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You did it, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. You can do it with a little more effort, don't give up!");
+        }
     }
 }
